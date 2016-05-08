1c5a01f3a2ee6f3a1a8536efe9345abcfa455392
Amit Kulkarni <pythoncpp@gmail.com>
2016-05-09 00:26:03 +0530

    Initial Commit

---
+++ Minwain/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Minwain
+//
+//  Created by Amit Kulkarni on 09/05/16.
+//  Copyright © 2016 Amit Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Minwain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Minwain
+//
+//  Created by Amit Kulkarni on 09/05/16.
+//  Copyright © 2016 Amit Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
